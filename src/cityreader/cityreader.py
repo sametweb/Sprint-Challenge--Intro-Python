a06c0f33a61eea61bc19462a0a7aec46ddbae3c5
sametweb <sametmutevellioglu@gmail.com>
2020-07-10 10:17:35 -0700

    stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,11 +1,22 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+
+
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    # def __repr__(self):
+    #     return f'("{self.name}", {self.lat}, {self.lon})'
 
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader` function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -16,26 +27,35 @@
 # should not be loaded into a City object.
 cities = []
 
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
+    # TODO Implement the functionality to read from the 'cities.csv' file
+    # For each city record, create a new City instance and add it to the
+    # `cities` list
+
+    with open('./cities.csv', newline='') as csvfile:
+        my_reader = csv.reader(csvfile)
+        next(my_reader)
+
+        for row in my_reader:
+            cities.append(City(str(row[0]), float(row[3]), float(row[4])))
+
     return cities
+
 
 cityreader(cities)
 
-# Print the list of cities (name, lat, lon), 1 record per line.
+# # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print(f'({c.name}, {c.lat}, {c.lon})')
 
 # STRETCH GOAL!
 #
 # Allow the user to input two points, each specified by latitude and longitude.
-# These points form the corners of a lat/lon square. Pass these latitude and 
+# These points form the corners of a lat/lon square. Pass these latitude and
 # longitude values as parameters to the `cityreader_stretch` function, along
 # with the `cities` list that holds all the City instances from the `cityreader`
-# function. This function should output all the cities that fall within the 
+# function. This function should output all the cities that fall within the
 # coordinate square.
 #
 # Be aware that the user could specify either a lower-left/upper-right pair of
@@ -59,13 +79,39 @@ for c in cities:
 # Salt Lake City: (40.7774,-111.9301)
 
 # TODO Get latitude and longitude values from the user
+first = input('Enter lat1,lon1: ')
+second = input('Enter lat2,lon2: ')
+
+lat1, lon1 = first.split(',')
+lat2, lon2 = second.split(',')
+lat1, lon1, lat2, lon2 = float(lat1), float(lon1), float(lat2), float(lon2)
+
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
+    # within will hold the cities that fall within the specified region
+    within = []
 
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+    # TODO Ensure that the lat and lon valuse are all floats
+    # Go through each city and check to see if it falls within
+    # the specified coordinates.
+    north, south, east, west = 0, 0, 0, 0
 
-  return within
+    if lat1 > lat2:
+        north = lat1
+        south = lat2
+    elif lat1 < lat2:
+        north = lat2
+        south = lat1
+
+    if lon1 > lon2:
+        east = lon1
+        west = lon2
+    elif lon1 < lon2:
+        east = lon2
+        west = lon1
+
+    for city in cities:
+        if city.lat < north and city.lat > south and city.lon < east and city.lon > west:
+            within.append(city)
+
+    return within
